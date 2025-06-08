e72cdcedb815e6eef43b74c966d5b3bf16f09675
wfawkes <josewellisondossantos2@gmail.com>
2025-06-08 15:21:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 opinions = []
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        opinion = request.form.get('opinion')
+        if opinion:
+            opinions.append(opinion)
     return render_template('index.html')
-
-@app.route('/enviar', methods=['POST'])
-def enviar():
-    opiniao = request.form['opiniao']
-    if opiniao:
-        opinions.append(opiniao)
-    return redirect('/')
 
 @app.route('/admin')
 def admin():
-    return "<br>".join(opinions)
+    return render_template('admin.html', opinions=opinions)
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
